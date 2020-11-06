13887fc684dd23c19eb1a6e50454f28f14a426c0
root <root@admin.hl.local>
2020-11-06 00:08:52 -0800

    First commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# youtube-ad-domains-pihole
+
+##### How to generate list on your own machine:
+###### Requires:
+- Sublist3r
+- pihole
+- run as root
+
+##### Or you can import it:
+I commit the adlist I generate on my own daily to this repo.
+
+You can import it into your pihole through the web interface. Goto Group Management Menu > Adlist page. And add this url () as a new adlist.
---
+++ generate-domains.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash 
+# Generates a list of youtube ad domains using sublist3r to block in pihole.
+# This is meant to be run by crontab.
+# -------------------------------------------------------------------------------------
+# Blocking ads on YouTube is hit or miss with Pi-hole. The reason for this is 
+# because ads are typically served from the same domain as the video is.
+
+# Many users see unique domains in the logs such as 
+# r4---sn-vgqs7nez.googlevideo.com and have had some success in blocking these,
+# but there are so many and it's different for different people, so it's not foolproof.
+# -------------------------------------------------------------------------------------
+
+SUBLIST3R=/opt/Sublist3r/sublist3r.py
+
+# PATH where http://pi.hole is hosted
+PIHOLE_WWW_PATH=/var/www/html
+
+$SUBLIST3R -d googlevideo.com -n -o /tmp/domains.txt
+cat /tmp/domains.txt >> youtube-ad-domains.txt
+echo "$(grep  '^r' youtube-ad-domains.txt | grep -v '^redirector' | sort | uniq)" > youtube-ad-domains.txt
+cp youtube-ad-domains.txt $PIHOLE_WWW_PATH/youtube-ad-domains.txt
+
+pihole -g
+pihole restartdns
+
